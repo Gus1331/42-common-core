3fae9b30d90d805a2ffbd1fbc6cb31ae71052ab0
Gus1331 <gustavooliver.souza@gmail.com>
2025-07-20 17:58:31 -0300

    feat: add ft_atoi ft_isalpha

---
+++ libft/ft_atoi.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gustaoli <gustaoli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 00:56:50 by gustaoli          #+#    #+#             */
+/*   Updated: 2025/07/20 17:56:22 by gustaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	skip_spaces(char **str);
+int		is_num(char c);
+int		is_negative(char **str);
+int		ft_atoi(char *str);
+
+int	ft_atoi(char *str)
+{
+	int	negative;
+	int	res;
+
+	skip_spaces(&str);
+	negative = is_negative(&str);
+	res = 0;
+	while (is_num(*str))
+	{
+		res *= 10;
+		res += *str - '0';
+		str++;
+	}
+	if (negative)
+	{
+		return (res * -1);
+	}
+	return (res);
+}
+
+void	skip_spaces(char **str)
+{
+	while (**str == ' ' || (**str >= '\t' && **str <= '\f'))
+	{
+		(*str)++;
+	}
+}
+
+int	is_num(char c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int	is_negative(char **str)
+{
+	int	res;
+
+	res = 0;
+	if (**str == '-' || **str == '+')
+	{
+		if (**str == '-')
+		{
+			res++;
+		}
+		(*str)++;
+	}
+	return (res);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	printf("%i\n", ft_atoi("    \t -+---+-+324fd243"));
+	printf("%i\n", ft_atoi("  ---+--+1234ab567"));
+}
+*/
